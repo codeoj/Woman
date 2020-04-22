d720055a919fcf99090b7a390f22789213e27611
Oliver <oliverjgodoy@gmail.com>
2020-04-22 17:10:48 -0300

    pullrequest com o projeto

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,13 @@
+//Importa as funcionalidades do express (npm install express) 
+//Importa o modulo express pra dentro da variavel const express
 const express = require('express');
+
 const app = express();
 
 const port = 3333;
 
-app.get('/', (req, res) => res.send('Hello World'));
+//Cria a rota. ('/rota') => É o endereço da rota. ('/') => Rota raiz.
+app.get('/', (req, res) => res.send('Hello World')); //A resposta pode ser: response.json({}) obejeto chave valor
 
+//Ao executar fica ouvindo as requisições no navegador (localhost:port)
 app.listen(port, () => console.log(`Servidor iniciado em http://localhost:${port}`));
